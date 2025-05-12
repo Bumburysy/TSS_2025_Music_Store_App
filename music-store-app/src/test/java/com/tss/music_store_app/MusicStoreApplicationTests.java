1175fc0286289db1977bd094f0d343cbd0d0f8bb
Michau <botkotel@gmail.com>
2025-05-12 15:17:40 +0200

    Project Initialization

---
+++ music-store-app/src/test/java/com/tss/music_store_app/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.music_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
