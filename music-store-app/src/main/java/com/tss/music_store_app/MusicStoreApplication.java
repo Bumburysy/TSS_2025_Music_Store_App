cd0961e958be3a9ce107b9b5d78f3ff084de0fd4
Michau <botkotel@gmail.com>
2025-05-13 13:05:58 +0200

    Enabled Mongo Repositories

--- music-store-app/src/main/java/com/tss/music_store_app/MusicStoreApplication.java
+++ music-store-app/src/main/java/com/tss/music_store_app/MusicStoreApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.tss"})
 @EnableAutoConfiguration
+@EnableMongoRepositories(basePackages = "com.tss.repositories")
 public class MusicStoreApplication {
 
 	public static void main(String[] args) {
