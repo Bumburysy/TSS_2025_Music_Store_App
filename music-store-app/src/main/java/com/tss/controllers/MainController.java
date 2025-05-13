1fd93da00e1ec5a5dc751d314546135ce994d7d3
Michau <botkotel@gmail.com>
2025-05-13 13:13:21 +0200

    Added mapping for users and stock

--- music-store-app/src/main/java/com/tss/controllers/MainController.java
+++ music-store-app/src/main/java/com/tss/controllers/MainController.java
@@ -6,15 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    // Home Page
     @GetMapping("/")
     public String home() {
         return "index"; 
     }
+         
+    @GetMapping("/albums")
+    public String albums() {
+        return "albums";
+    }
     
-    // Login Page
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+    
+    @GetMapping("/stock")
+    public String stock() {
+        return "stock";
     }
 }
