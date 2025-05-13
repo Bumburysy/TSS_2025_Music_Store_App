5e256c7c4e3be9fb7f62534ea72afeabb61d24db
Michau <botkotel@gmail.com>
2025-05-13 13:12:30 +0200

    Rewritten SecurityFilterChain function Added temporary test users Added passwordEncoder function

--- music-store-app/src/main/java/com/tss/config/SecurityConfig.java
+++ music-store-app/src/main/java/com/tss/config/SecurityConfig.java
@@ -1,27 +1,59 @@
 package com.tss.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/login", "/register").permitAll()  // dostęp bez logowania
-                .anyRequest().authenticated()  // reszta tylko po zalogowaniu
-            .and()
-            .formLogin()
-                .loginPage("/login")
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/logout").permitAll()
+                .antMatchers("/albums/**", "/stock/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/users/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin() 
                 .defaultSuccessUrl("/", true)
                 .permitAll()
-            .and()
+                .and()
             .logout()
+                .logoutSuccessUrl("/")
                 .permitAll();
+        return http.build();
+    }
+    
+    // Testowi użytwkonicy w pamięci
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("user123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
