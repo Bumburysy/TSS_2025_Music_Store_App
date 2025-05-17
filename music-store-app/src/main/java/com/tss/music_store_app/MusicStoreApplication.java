20912e671b324dd8eecb69cb489080b8fb70b6ca
Michau <botkotel@gmail.com>
2025-05-17 17:26:43 +0200

    Added EntityScan and TransactionManagement

--- music-store-app/src/main/java/com/tss/music_store_app/MusicStoreApplication.java
+++ music-store-app/src/main/java/com/tss/music_store_app/MusicStoreApplication.java
@@ -3,13 +3,17 @@ package com.tss.music_store_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.tss"})
 @EnableAutoConfiguration
 @EnableMongoRepositories(basePackages = "com.tss.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.tss.entities")
 public class MusicStoreApplication {
 
 	public static void main(String[] args) {
